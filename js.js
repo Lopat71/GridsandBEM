1f9840862634487d129d4f8a0904639c6b72e13f
Artem Terekhov <opa4941@gmail.com>
2024-07-01 16:47:53 +0400

    Block-4

---
+++ js.js
@@ -0,0 +1,9 @@
+$("#header__btn").click(function () {
+  $("#aside").toggleClass("visible");
+  $("#main").toggleClass("visible");
+});
+
+$("#aside__burger").click(function () {
+  $("#aside").toggleClass("visible");
+  $("#main").toggleClass("visible");
+});
